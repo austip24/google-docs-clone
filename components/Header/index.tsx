0614bf62cf6fa03722c16c424f52253007182c2a
austip24 <austinpier24@gmail.com>
2022-06-02 06:36:44 -0700

    initialize tailwindcss

---
+++ components/Header/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface HeaderProps {}
+
+const Header: React.FC<HeaderProps> = () => {
+	return <div className="flex items-center">Header</div>;
+};
+
+export default Header;
