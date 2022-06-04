6af77bb1ce1fb18987d707a2ededcada5cec817d
austip24 <austinpier24@gmail.com>
2022-06-03 19:03:15 -0700

    specified that hook is only meant for client side rendering

--- hooks/useMediaQuery.ts
+++ hooks/useMediaQuery.ts
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react";
 
+// Note: This is only meant to be used for client side rendering
+// and will not work for server side rendering.
 const useMediaQuery = (query: string): boolean => {
 	const [matches, setMatches] = useState(false);
 
 	useEffect(() => {
 		const matchQueryList = window.matchMedia(query);
 		const handleChange = (e: any) => setMatches(e.matches);
+
 		matchQueryList.addEventListener("change", handleChange);
+
 		return () => matchQueryList.removeEventListener("change", handleChange);
 	}, [query]);
 
